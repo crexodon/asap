3b0424a08a18530beba0e08bbac60786ecd30ab3
norikaschneider <norika.schneider@gmail.com>
2026-02-11 02:57:38 +0100

    small changes robot_node stillnot launchable

--- planner_ws/src/warehouse_robot/setup.py
+++ planner_ws/src/warehouse_robot/setup.py
@@ -1,23 +1,25 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'warehouse_robot'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/warehouse_robot.launch.py']),
+
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Noris_ASAP',
-    maintainer_email='n/a',
+    maintainer='norikaschneider',
+    maintainer_email='noschneide@stud.hs-heilbronn.com',
     description='Discrete training robot node implementing /planner/cmd without Gazebo.',
-    license='MIT',
-    tests_require=['pytest'],
+    license='Apache-2.0',
     entry_points={
         'console_scripts': [
             'robot_node = warehouse_robot.robot_node:main',
