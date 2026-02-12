d4f7f4f5feac2e00d33368d2ac7a4828e7ee5e3c
crexodon <github@crexy.moe>
2026-02-12 05:10:31 +0100

    updated battery_controller with callable set_charging and subscribable is_charging

--- robot_ws/src/robot_hardware/robot_hardware/battery_controller.py
+++ robot_ws/src/robot_hardware/robot_hardware/battery_controller.py
@@ -1,8 +1,9 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.action import ActionServer
+from rclpy.callback_groups import ReentrantCallbackGroup
 from sensor_msgs.msg import BatteryState
 from std_msgs.msg import Bool
+from std_srvs.srv import SetBool
 import math
 
 class BatterySimulator(Node):
@@ -26,11 +27,9 @@ class BatterySimulator(Node):
 
         self.capacity_wh = 100.0  # current capacity
         self.is_charging = False
-
         self.last_update_time = self.get_clock().now()
 
-        self.last_cmd_time = self.get_clock().now()
-
+        # Publishes Battery State
         self.battery_state_pub = self.create_publisher(
             BatteryState,
             '/robot/battery_status',
@@ -43,25 +42,68 @@ class BatterySimulator(Node):
             10
         )
 
-        self.timer = self.create_timer(
-            1.0 / self.publish_rate,
-            self.timer_callback
+        self.set_charging_srv = self.create_service(
+            SetBool,
+            '/robot/set_charging',
+            self.set_charging_callback,
+            callback_group=self.cb_group
         )
 
+        self.timer = self.create_timer(
+            1.0 / self.publish_rate,
+            self.timer_callback,
+            callback_group=self.cb_group
+        )
+
+        self.get_logger().info('BatterySimulator started')
+
+    def get_battery_percentage(self) -> float:
+        return self.capacity_wh / self.max_capacity
+
+    def set_charging_callback(self, request, response):
+        """Service callback to enable/disable charging."""
+        self.is_charging = request.data
+        response.success = True
+        response.message = f"Charging {'enabled' if self.is_charging else 'disabled'}"
+        self.get_logger().info(response.message)
+        return response
+    
     def timer_callback(self):
         current_time = self.get_clock().now()
-        time_since_cmd = (current_time - self.last_cmd_time).nanoseconds / 1e9
+        time_since_last_update = (current_time - self.last_update_time).nanoseconds / 1e9
 
-        if time_since_cmd > self.battery_ticks:
-            time_since_update = (current_time - self.last_update_time).nanoseconds / 1e9
-        
-        # if time_since_cmd > self.battery_ticks and not is_charging
-        # decrease battery until 0 by discharge_rate
+        # Check if a battery tick has elapsed
+        if time_since_last_update >= self.battery_ticks:
+            self.last_update_time = current_time
 
-        # if time_since_cmd > self.battery_ticks and is_charging
-        # increase battery until max_capacity by charging_rate
-    
-        # publish current battery level with battery_state_pub
+            if self.is_charging:
+                self.capacity_wh = min(
+                    self.capacity_wh + self.charging_rate,
+                    self.max_capacity
+                )
+            else:
+                self.capacity_wh = max(
+                    self.capacity_wh - self.discharging_rate,
+                    0.0
+                )
+
+        # Publish current battery state
+        msg = BatteryState()
+        msg.header.stamp = current_time.to_msg()
+        msg.percentage = self.get_battery_percentage()
+        msg.capacity = self.capacity_wh
+        msg.design_capacity = self.max_capacity
+        msg.power_supply_status = (
+            BatteryState.POWER_SUPPLY_STATUS_CHARGING if self.is_charging
+            else BatteryState.POWER_SUPPLY_STATUS_DISCHARGING
+        )
+        msg.present = True
+        self.battery_state_pub.publish(msg)
+
+        # Publish charging state
+        charging_msg = Bool()
+        charging_msg.data = self.is_charging
+        self.is_charging_pub.publish(charging_msg)
 
 def main(args=None):
     rclpy.init(args=args)
